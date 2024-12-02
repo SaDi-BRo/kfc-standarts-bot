2212c11ebfbb2e45f7356378fe3499965c6ae405
SaDi <sayidullohabbasov0909@gmail.com>
2024-12-03 02:57:10 +0500

    Update run.ts

--- src/core/run.ts
+++ src/core/run.ts
@@ -1,13 +1,20 @@
-import { webhookCallback, config } from './lib.ts';
+import { webhookCallback, serve } from './lib.ts';
 import { bot } from './bot.ts';
 
-Deno.env.set('MODE', Deno.env.get('MODE') || config().MODE);
-
-Deno.env.get('MODE') === 'development' && bot.start();
-
-import { Application } from 'https://deno.land/x/oak@v17.1.3/mod.ts';
-
-const app = new Application(); // or whatever you're using
-
-// Make sure to specify the framework you use.
-Deno.env.get('MODE') === 'production' && app.use(webhookCallback(bot, 'oak'));
+if (Deno.env.get('MODE') === 'development') bot.start();
+if (Deno.env.get('MODE') === 'production') {
+  const handleUpdate = webhookCallback(bot, 'std/http');
+  serve(async req => {
+    if (req.method === 'POST') {
+      const url = new URL(req.url);
+      if (url.pathname.slice(1) === bot.token) {
+        try {
+          return await handleUpdate(req);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    }
+    return new Response();
+  });
+}
