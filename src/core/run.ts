fd65aa922255ae1ca5c6dc331f738695323533e7
SaDi-BRo <sayidullohabbasov0909@gmail.com>
2024-12-02 05:30:51 +0500

    edited webhook source

--- src/core/run.ts
+++ src/core/run.ts
@@ -1,11 +1,13 @@
 import { webhookCallback, config } from './lib.ts';
 import { bot } from './bot.ts';
 
-config().MODE === 'development' && bot.start();
+Deno.env.set('MODE', Deno.env.get('MODE') || config().MODE);
+
+Deno.env.get('MODE') === 'development' && bot.start();
 
 import { Application } from 'https://deno.land/x/oak@v17.1.3/mod.ts';
 
 const app = new Application(); // or whatever you're using
 
 // Make sure to specify the framework you use.
-app.use(webhookCallback(bot, 'oak'));
+Deno.env.get('MODE') === 'development' && app.use(webhookCallback(bot, 'oak'));
