21f2128b7be878741594e451065489bfb326881a
SaDi-BRo <sayidullohabbasov0909@gmail.com>
2024-12-02 05:14:50 +0500

    edited

--- src/core/bot.ts
+++ src/core/bot.ts
@@ -1,5 +1,7 @@
 import { Bot, config } from './lib.ts';
 
-const bot = new Bot(config().BOT_TOKEN);
+Deno.env.set('BOT_TOKEN', Deno.env.get('BOT_TOKEN') || config().BOT_TOKEN);
+
+const bot = new Bot(Deno.env.get('BOT_TOKEN')!);
 
 export { bot };
